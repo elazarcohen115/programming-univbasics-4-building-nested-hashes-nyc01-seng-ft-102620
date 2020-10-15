735fb01122df3449715f7f771194c20f60ef2239
simon cohen <escohen115@gmail.com>
2020-10-15 00:02:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,13 @@ end
 
 def monopoly_with_second_tier
   {
-    :railroads => {
+    :railroads => 
+    {
       pieces: 4,
-      rent_in_dollars: { } 
-      names: { }
+      rent_in_dollars: { 
+      } 
+      names: { 
+      }
     }
   }
 
